5341282d223009f12c26f9947093c5c6f41d54a8
Aleydon <aleydonroberto@gmail.com>
2021-03-21 18:14:30 -0300

    Ajusted counter for eslint/prettier

--- src/Components/counter.js
+++ src/Components/counter.js
@@ -1,38 +1,25 @@
 /* By: Roberto Aleydon */
 
 import React, {useState} from 'react';
-import { StatusBar, Text } from 'react-native';
+import {StatusBar, Text} from 'react-native';
 
 import Header from './Header';
-import { CenterCounter, FabView, AddText } from './style';
-
-
-
-
-
+import {CenterCounter, FabView, AddText} from './style';
 
 export default function Counter() {
-    const [ counter, setCounter ] = useState(0);
+  const [counter, setCounter] = useState(0);
 
-
-    return (
-        <>
-            <StatusBar backgroundColor="#007fff"/>
-            <Header />
-
-            <CenterCounter>
-                <Text>You have pushed the button this many times</Text>
-                <Text style={{fontSize: 25}}>{counter}</Text>
-            </CenterCounter>
-
-            <FabView 
-                activeOpacity={0.6}
-                onPress={() => setCounter(counter + 1)}>
-                <AddText>+</AddText>
-            </FabView>
-        </>
-);
-};
-
-
-
+  return (
+    <>
+      <StatusBar backgroundColor="#007fff" />
+      <Header />
+      <CenterCounter>
+        <Text>You have pushed the button this many times</Text>
+        <Text style={{fontSize: 25}}>{counter}</Text>
+      </CenterCounter>
+      <FabView activeOpacity={0.6} onPress={() => setCounter(counter + 1)}>
+        <AddText>+</AddText>
+      </FabView>
+    </>
+  );
+}
